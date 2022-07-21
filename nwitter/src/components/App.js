06eedbd21e13afb439b0a6d4e187fcfefd4c5007
kyungchandevelop@gmail.com <championcom@naver.com>
2022-07-21 23:45:52 +0900

    파이어베이스auth설정

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router";
-
+import { authService } from "../firebase";
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Nwitter {new Date().getUTCFullYear()}</footer>
+    </>
+  );
 }
 export default App;
--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -3,9 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routers/Auth";
 import Home from "../routers/Home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(isLoggedIn);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Switch>
