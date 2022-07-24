4531a978228b08760b9cbd2074b168d9f97e8a38
kyungchandevelop@gmail.com <championcom@naver.com>
2022-07-24 19:37:52 +0900

    소셜로그인

--- nwitter/src/routers/Auth.js
+++ nwitter/src/routers/Auth.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { authService } from "../firebase";
+import { firebaseInstance } from "../firebase";
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
-
+  const [error, setError] = useState("");
   const LoginOnChange = (e) => {
     const {
       target: { name, value },
@@ -26,10 +27,23 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      alert(error);
+      setError(error.message);
     }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
 
+  const onSocialClick = async (e) => {
+    const {
+      target: { name },
+    } = e;
+    let provider;
+    name === "google"
+      ? (provider = new firebaseInstance.auth.GoogleAuthProvider())
+      : (provider = new firebaseInstance.auth.GithubAuthProvider());
+
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -49,11 +63,19 @@ const Auth = () => {
           value={password}
           onChange={LoginOnChange}
         />
+        {error}
+        <span onClick={toggleAccount}>
+          {newAccount ? "login" : "CreateAccount"}
+        </span>
         <input type="submit" value={newAccount ? "CreateAccount" : "Log in"} />
       </form>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with GitHub</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with GitHub
+        </button>
       </div>
     </div>
   );
