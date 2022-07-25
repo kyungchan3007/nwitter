98a7b7d09d02dc9c7ec386493b33d7ddf7afae0c
kyungchandevelop@gmail.com <championcom@naver.com>
2022-07-25 16:40:27 +0900

    firebaseDatabase

--- nwitter/src/routers/Home.js
+++ nwitter/src/routers/Home.js
@@ -1,4 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="what's on your mind"
+          maxLength={120}
+          value={nweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
