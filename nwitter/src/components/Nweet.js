f7c510aa99653049fdb597f0731977cc86f8abfe
kyungchandevelop@gmail.com <championcom@naver.com>
2022-08-01 14:40:53 +0900

    게시글 삭제

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { dbService } from "../firebase";
 
-const Nweet = ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete Nweet</button>
-        <button>Edit Nweet</button>
-      </>
-    )}
-  </div>
-);
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+
+  return (
+    <div>
+      <h4>{nweetObj.text}</h4>
+      {isOwner && (
+        <>
+          <button onClick={onDeleteClick}>Delete Nweet</button>
+          <button>Edit Nweet</button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Nweet;
